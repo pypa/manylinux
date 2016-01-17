1845405c7da281dd840696dafe2a51be21a8ac06
Nathaniel J. Smith <njs@pobox.com>
2016-01-17 01:40:36 -0800

    More fine-grained analysis of canopy
    canopy-1.6.1-libraries-needed-for-installer:
    libraries needed by installer, not provided by installer
    
    canopy-1.6.1-libraries-needed-for-main-env:
    libraries needed by main environment, not provided by *either*
    installer *or* main environment
    
    canopy-1.6.1-libraries-needed-for-either:
    libraries needed by something in at least one environment, and not
    provided by either environment. (Requires a slightly tweaked
    external-so.sh). Probably meaningless.

--- policy-info/external-so.sh
+++ policy-info/external-so.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-# Usage: $0 <list of directory names>
+# Usage: $0 MAIN-DIR [SUPP-DIR1 ...]
 
-# Prints a list of all shared libraries that are *needed* by some file in the
-# given directories, but are not *provided* in the given directories.
+# Prints a list of all shared libraries that are *needed* by some file in
+# MAIN-DIR, but are not *provided* by either the MAIN-DIR or any of the
+# SUPP-DIRs.
 
 # First pipeline finds all ELF files, then uses readelf to extract list of
 # NEEDED objects (printed like 'blah blah [actual .so name]'), and extracts
@@ -17,7 +18,7 @@
 # with symlinks between them, so we accept multiple directory names, and use
 # -L to follow symlinks, just to be sure.
 comm -23 \
-     <(find -L "$@"                                               \
+     <(find -L "$1"                                               \
             -type f -a -exec sh -c "file '{}' | grep -q ELF" \;   \
             -print0                                               \
        | xargs -0 -I FILE readelf -d FILE                         \
