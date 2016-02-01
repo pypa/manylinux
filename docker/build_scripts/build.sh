ce7030616eee792abca10ace6165b386b8e54946
Robert McGibbon <rmcgibbo@gmail.com>
2016-01-31 16:15:08 -0800

    Add package diffutils (for diff command, used in python's ./configure) and supply an extra gcc flag

--- docker/build_scripts/build.sh
+++ docker/build_scripts/build.sh
@@ -32,7 +32,7 @@ rpm -Uvh --replacepkgs epel-release-5*.rpm
 rm -f epel-release-5*.rpm
 
 # Development tools and libraries
-yum -y install bzip2 make git patch unzip bison yasm \
+yum -y install bzip2 make git patch unzip bison yasm diffutils \
     autoconf automake which file \
     kernel-devel-`uname -r` \
     devtoolset-2-binutils devtoolset-2-gcc \
