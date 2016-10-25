cc1a8a36a16830f44d021886ca5826fc354e3bd8
Cody Wilbourn <cody@parsely.com>
2016-10-25 16:40:08 -0400

    Set the latest tag on images

--- docker/deploy.sh
+++ docker/deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 docker login -e $DOCKER_EMAIL -u $DOCKER_USERNAME -p $DOCKER_PASSWORD 
+TAG=`if [ "$TRAVIS_BRANCH" == "master" ]; then echo "latest"; else echo $TRAVIS_BRANCH ; fi`
 docker tag $REPO:$COMMIT $REPO:$TAG
 docker tag $REPO:$COMMIT $REPO:travis-$TRAVIS_BUILD_NUMBER
 docker push $REPO
