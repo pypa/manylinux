c44f007a01fc8cb0ff6fdadafdc8c4b7ba93c158
Dan Blanchard <dan.blanchard@gmail.com>
2016-12-15 09:48:39 -0500

    Build prerelease wheels one at a time to prevent stupid broken distributions taking the whole thing down

--- build-wheels.sh
+++ build-wheels.sh
@@ -16,8 +16,10 @@ for PYBIN in /opt/python/*/bin; do
     else
          CFLAGS="-I/usr/local/ssl/include" LDFLAGS="-L/usr/local/ssl/lib" ${PYBIN}/pip wheel cryptography -w /io/wheelhouse/ -f /io/wheelhouse
          ${PYBIN}/pip wheel -r /io/dev-requirements.txt -w /io/wheelhouse/ -f /io/wheelhouse || true
-         # Do another run allowing dev builds
-         ${PYBIN}/pip wheel --pre -r /io/dev-requirements.txt -w /io/wheelhouse/ -f /io/wheelhouse || true
+         # Do another run allowing dev builds, and do it with a separate run per
+         # requirement so that one broken prerelease doesn't stop the rest from
+         # being build---I'm looking at *you* statsmodel 0.8.0rc1
+         cat /io/dev-requirements.txt | tr '\n' '\0' | xargs -0 ${PYBIN}/pip wheel --pre -w /io/wheelhouse/ -f /io/wheelhouse || true
     fi
 done
 
