5d69b6ed68ce9d6d6e4df7ac1072567cf7048bee
Olivier Grisel <olivier.grisel@ensta.org>
2016-12-30 17:44:26 +0100

    Split travis build into 2 parallel jobs

--- docker/deploy.sh
+++ docker/deploy.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 docker login -e noemail -u $QUAY_USERNAME -p $QUAY_PASSWORD quay.io
-for tag in quay.io/pypa/manylinux1_i686 quay.io/pypa/manylinux1_x86_64; do
-    docker tag ${tag}:${TRAVIS_COMMIT} ${tag}:latest
-    docker push ${tag}:latest
-done
+tag="quay.io/pypa/manylinux1_$PLATFORM"
+docker tag ${tag}:${TRAVIS_COMMIT} ${tag}:latest
+docker push ${tag}:latest
