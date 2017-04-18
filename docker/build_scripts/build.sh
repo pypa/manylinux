d09af6f76ad340f5549473dd6633db04c8206005
ofek <ofekmeister@gmail.com>
2017-04-18 13:44:48 -0400

    fix command

--- docker/build_scripts/build.sh
+++ docker/build_scripts/build.sh
@@ -81,12 +81,8 @@ rm -rf sqlite-autoconf-3160200*
 build_openssl $OPENSSL_ROOT $OPENSSL_HASH
 mkdir -p /opt/python
 build_cpythons $CPYTHON_VERSIONS
-get_pypys $PYPY_VERSION_TAGS
-#if [ arch == "x86_64" ]; then
-#    get_pypys $PYPY_VERSION_TAGS
-#fi
 
-if [ arch == "x86_64" ]; then
+if [ `arch` == "x86_64" ]; then
     get_pypys $PYPY_VERSION_TAGS
 fi
 
