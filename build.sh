02323ff0ba2af896e492e6b2b26627e9e8ac67b5
Dan Blanchard <dan.blanchard@gmail.com>
2018-02-14 12:11:12 -0500

    Switch to TRAVIS_COMMIT in .travis.yml

--- build.sh
+++ build.sh
@@ -4,5 +4,5 @@
 set -ex
 
 docker/build_scripts/prefetch.sh openssl curl
-docker build --rm -t $REPO:$COMMIT -f docker/Dockerfile-x86_64 docker/
+docker build --rm -t $REPO:$TRAVIS_COMMIT -f docker/Dockerfile-x86_64 docker/
 docker system prune -f
