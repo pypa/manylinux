87886d4e40823d09ebd1ddfd06b5b167e51af435
Dominik Lang <langdominik@protonmail.com>
2018-05-03 18:16:56 +0200

    Adapt build.sh for the x86_64 two-stage build
    - Add platform-dependent clause
    - Assign the same prefix to the the no-vsyscall image (quay.io/pypa/...)
    - The dependent docker/Dockerfile-x86_64 needs this prefix in FROM ...

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
 docker/build_scripts/prefetch.sh openssl curl
-docker build --rm -t quay.io/pypa/manylinux1_$PLATFORM:$TRAVIS_COMMIT -f docker/Dockerfile-$PLATFORM docker/
+if [ $PLATFORM == x86_64 ]; then
+    docker build --rm -t quay.io/pypa/manylinux2010_centos-6.9-no-vsyscall -f docker/glibc/Dockerfile docker/glibc/
+fi
+docker build --rm -t quay.io/pypa/manylinux2010_$PLATFORM:$TRAVIS_COMMIT -f docker/Dockerfile-$PLATFORM docker/
 docker system prune -f
