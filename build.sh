0236569937e433bfe0eaa4a897e4fb752cb9d80c
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2018-12-26 10:39:18 -0200

    Remove log for building glibc

--- build.sh
+++ build.sh
@@ -5,7 +5,8 @@ set -ex
 
 docker/build_scripts/prefetch.sh openssl curl
 if [ $PLATFORM == x86_64 ]; then
-    docker build --rm -t quay.io/pypa/manylinux2010_centos-6-no-vsyscall -f docker/glibc/Dockerfile docker/glibc/
+    echo "Building quay.io/pypa/manylinux2010_centos-6-no-vsyscall"
+    docker build --rm -t quay.io/pypa/manylinux2010_centos-6-no-vsyscall -f docker/glibc/Dockerfile docker/glibc/ > /dev/null
 fi
 docker build --rm -t quay.io/pypa/manylinux2010_$PLATFORM:$TRAVIS_COMMIT -f docker/Dockerfile-$PLATFORM docker/
 docker system prune -f
