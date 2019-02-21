4daf244a7552c3a7c3c9894bf37e9f52181e1dc9
mayeut <mayeut@users.noreply.github.com>
2019-02-21 22:10:38 +0100

    Do not call docker system prune

--- build.sh
+++ build.sh
@@ -5,4 +5,3 @@ set -ex
 
 docker/build_scripts/prefetch.sh openssl curl
 docker build --rm -t quay.io/pypa/manylinux1_$PLATFORM:$TRAVIS_COMMIT -f docker/Dockerfile-$PLATFORM docker/
-docker system prune -f
