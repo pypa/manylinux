6765234baa7f43a808832fe1e1c744a28714d579
mayeut <mayeut@users.noreply.github.com>
2019-04-05 22:41:25 +0200

    Replace travis wait by custom command
    Prevent stdout timeout. The global travis timeout is the only one to apply now.

--- build.sh
+++ build.sh
@@ -5,6 +5,10 @@ set -ex
 
 docker/build_scripts/prefetch.sh openssl curl
 if [ $PLATFORM == x86_64 ]; then
+    # Output something every 10 minutes or Travis kills the job
+    while sleep 9m; do echo -n -e " \b"; done &
     docker build --rm -t quay.io/pypa/manylinux2010_centos-6-no-vsyscall -f docker/glibc/Dockerfile docker/glibc/
+    # Killing background sleep loop
+    kill %1
 fi
 docker build --rm -t quay.io/pypa/manylinux2010_$PLATFORM:$TRAVIS_COMMIT -f docker/Dockerfile-$PLATFORM docker/
