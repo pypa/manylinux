72c1e7f7654979223a9adfd38974b0e8638095b6
mayeut <mayeut@users.noreply.github.com>
2019-07-16 22:21:16 +0200

    Update python from 3.6.8/3.7.3 to 3.6.9/3.7.4

--- docker/build_scripts/build_env.sh
+++ docker/build_scripts/build_env.sh
@@ -1,7 +1,7 @@
 # source me
 
 PYTHON_DOWNLOAD_URL=https://www.python.org/ftp/python
-CPYTHON_VERSIONS="2.7.16 3.4.10 3.5.7 3.6.8 3.7.3"
+CPYTHON_VERSIONS="2.7.16 3.4.10 3.5.7 3.6.9 3.7.4"
 
 # openssl version to build, with expected sha256 hash of .tar.gz
 # archive.
