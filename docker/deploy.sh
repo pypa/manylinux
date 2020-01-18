4e26672fa6d251cdfc8e22c486ee01e1a7bd2ffd
Joe Rickerby <joerick@mac.com>
2020-01-18 11:29:17 +0000

    Tag each docker build with a unique id, and push to quay.io

--- docker/deploy.sh
+++ docker/deploy.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-docker login -u $QUAY_USERNAME -p $QUAY_PASSWORD quay.io
 tag="quay.io/pypa/manylinux2010_$PLATFORM"
+build_id=$(git show -s --format=%cd-%h --date=short $TRAVIS_COMMIT)
+
+docker login -u $QUAY_USERNAME -p $QUAY_PASSWORD quay.io
+docker tag ${tag}:${TRAVIS_COMMIT} ${tag}:${build_id}
 docker tag ${tag}:${TRAVIS_COMMIT} ${tag}:latest
+docker push ${tag}:${build_id}
 docker push ${tag}:latest
