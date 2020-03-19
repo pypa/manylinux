c2a4706c057b0e48a9d08d6ad490f53df2802b9d
mattip <matti.picus@gmail.com>
2020-03-19 15:31:18 +0200

    pin python2.7 requirements

--- docker/build_scripts/build_utils.sh
+++ docker/build_scripts/build_utils.sh
@@ -61,7 +61,11 @@ function do_cpython_build {
     fi
     # Since we fall back on a canned copy of pip, we might not have
     # the latest pip and friends. Upgrade them to make sure.
-    ${prefix}/bin/pip install -U --require-hashes -r ${MY_DIR}/requirements.txt
+    if [ "${py_ver:0:1}" == "2" ]; then
+        ${prefix}/bin/pip install -U --require-hashes -r ${MY_DIR}/py27-requirements.txt
+    else
+        ${prefix}/bin/pip install -U --require-hashes -r ${MY_DIR}/requirements.txt
+    fi
     local abi_tag=$(${prefix}/bin/python ${MY_DIR}/python-tag-abi-tag.py)
     ln -s ${prefix} /opt/python/${abi_tag}
 }
