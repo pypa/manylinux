af4558f7da8b2f7be2cf28c03e6834d4c8b9021b
mephi42 <mephi42@gmail.com>
2020-04-29 21:58:14 +0200

    Build i686 glibc
    Fixes #561: this change fixes installing i686 packages into
    manylinux2010_x86_64 image.
    
    Also, it reworks Travis setup slightly, since x86_64 and i686 glibc
    builds should run in different stages in order to avoid timeouts.

---
+++ travis-ci/tags.sh
@@ -0,0 +1,22 @@
+case "${1-}" in
+32)
+    tags=( quay.io/pypa/manylinux2010_centos-6-with-vsyscall32:latest )
+    ;;
+64)
+    tags=( quay.io/pypa/manylinux2010_centos-6-with-vsyscall64:latest )
+    ;;
+combined)
+    tags=( quay.io/pypa/manylinux2010_centos-6-no-vsyscall:latest )
+    ;;
+all)
+    tags=(
+        quay.io/pypa/manylinux2010_centos-6-with-vsyscall32:latest
+        quay.io/pypa/manylinux2010_centos-6-with-vsyscall64:latest
+        quay.io/pypa/manylinux2010_centos-6-no-vsyscall:latest
+    )
+    ;;
+*)
+    echo "Usage: $0 {32|64|combined|all}" >&2
+    exit 1
+    ;;
+esac
