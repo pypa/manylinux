906e169ceff570e2d108aabeffbe9c249e4e8443
Michael Anselmi <manselmi16@massmutual.com>
2020-09-30 14:18:07 -0400

    [manylinux1] OpenSSL 1.1.1g → 1.1.1h
    Changelog: https://www.openssl.org/news/cl111.txt

--- docker/build_scripts/build_env.sh
+++ docker/build_scripts/build_env.sh
@@ -11,8 +11,8 @@ PERL_DOWNLOAD_URL=https://www.cpan.org/src/5.0
 
 # openssl version to build, with expected sha256 hash of .tar.gz
 # archive.
-OPENSSL_ROOT=openssl-1.1.1g
-OPENSSL_HASH=ddb04774f1e32f0c49751e21b67216ac87852ceb056b75209af2443400636d46
+OPENSSL_ROOT=openssl-1.1.1h
+OPENSSL_HASH=5c9ca8774bd7b03e5784f26ae9e9e6d749c9da2438545077e6b3d755a06595d9
 OPENSSL_DOWNLOAD_URL=https://www.openssl.org/source
 
 PATCHELF_VERSION=0.12
