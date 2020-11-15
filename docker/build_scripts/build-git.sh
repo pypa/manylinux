c2dffe88789c1261f521534545cedbad3418ced0
mayeut <mayeut@users.noreply.github.com>
2020-11-16 00:02:04 +0100

    Update git from 2.29.1 to 2.29.2

--- docker/build_scripts/build-git.sh
+++ docker/build_scripts/build-git.sh
@@ -11,8 +11,8 @@ MY_DIR=$(dirname "${BASH_SOURCE[0]}")
 source $MY_DIR/build_utils.sh
 
 # Install newest libtool
-GIT_ROOT=git-2.29.1
-GIT_HASH=728845a66103d8d1572a656123c2c09d7aa4c0ab8f4c3dc3911e14e18c85ee67
+GIT_ROOT=git-2.29.2
+GIT_HASH=869a121e1d75e4c28213df03d204156a17f02fce2dc77be9795b327830f54195
 GIT_DOWNLOAD_URL=https://www.kernel.org/pub/software/scm/git
 
 
