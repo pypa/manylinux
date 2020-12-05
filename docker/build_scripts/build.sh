394a958e69a0ff722d8a5cf250a14fba66a79655
mayeut <mayeut@users.noreply.github.com>
2020-12-05 10:41:42 +0100

    Use archives.fedoraproject.org for yum EPEL repos on manylinux2010
    Centos 6 is EOL and is no longer available from the usual mirrors, so switch
    to archives.fedoraproject.org for EPEL

--- docker/build_scripts/build.sh
+++ docker/build_scripts/build.sh
@@ -52,7 +52,9 @@ esac
 yum -y update
 
 # EPEL support (for cmake28 & yasm)
-yum -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-6.noarch.rpm curl
+yum -y install https://archives.fedoraproject.org/pub/archive/epel/6/x86_64/epel-release-6-8.noarch.rpm curl
+sed -i 's/^mirrorlist/#mirrorlist/g' /etc/yum.repos.d/*.repo
+sed -i 's;^#.*baseurl=http://download.fedoraproject.org/pub;baseurl=https://archives.fedoraproject.org/pub/archive;g' /etc/yum.repos.d/*.repo
 
 case $AUDITWHEEL_ARCH in
 x86_64)
