f6c6ee3a58fbda127fb8538cd10bfde2771e270d
Matthieu Darbois <mayeut@users.noreply.github.com>
2020-12-10 11:26:39 +0100

    [manylinux2014] workaround mirror issue on s390x (#868)
    Do not update epel-release package for now
    
    c.f. nealef/clefos#5

--- docker/build_scripts/build.sh
+++ docker/build_scripts/build.sh
@@ -50,7 +50,15 @@ sed -i '/^override_install_langs=/d' /etc/yum.conf
 # clean all in your Dockerfile, or otherwise address any potential security
 # concerns."
 # Decided not to clean at this point: https://github.com/pypa/manylinux/pull/129
-yum -y update
+
+if [ "${AUDITWHEEL_ARCH}" == "s390x" ]; then
+    # workaround for https://github.com/nealef/clefos/issues/5
+    # this shall be removed ASAP
+    yum -y install epel-release-7-12
+    yum -y --exclude=epel-release update
+else
+    yum -y update
+fi
 yum -y install yum-utils curl
 yum-config-manager --enable extras
 
