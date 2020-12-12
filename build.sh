7c67e05d89779fef2baa7a6e0556e5097952b607
mayeut <mayeut@users.noreply.github.com>
2020-12-12 10:25:57 +0100

    [manylinux2014] Use a single Dockerfile for all images
    This will ease adding new policies / platforms in the future (e.g. for PEP600 support with manylinux_2_24 images) using a single Dockerfile whatever the policy / platform.

--- build.sh
+++ build.sh
@@ -1,7 +1,53 @@
 #!/bin/bash
 
 # Stop at any error, show all commands
-set -ex
+set -exuo pipefail
 
+# Export variable needed by 'docker build --build-arg'
+export POLICY
+export PLATFORM
 
-docker build --rm -t quay.io/pypa/manylinux2014_$PLATFORM:$TRAVIS_COMMIT -f docker/Dockerfile-$PLATFORM docker/
+# get docker default multiarch image prefix for PLATFORM
+if [ "${PLATFORM}" == "x86_64" ]; then
+	MULTIARCH_PREFIX="amd64/"
+elif [ "${PLATFORM}" == "i686" ]; then
+	MULTIARCH_PREFIX="i386/"
+elif [ "${PLATFORM}" == "aarch64" ]; then
+	MULTIARCH_PREFIX="arm64v8/"
+elif [ "${PLATFORM}" == "ppc64le" ]; then
+	MULTIARCH_PREFIX="ppc64le/"
+elif [ "${PLATFORM}" == "s390x" ]; then
+	MULTIARCH_PREFIX="s390x/"
+else
+	echo "Unsupported platform: '${PLATFORM}'"
+	exit 1
+fi
+
+# setup BASEIMAGE and its specific properties
+if [ "${POLICY}" == "manylinux2014" ]; then
+	if [ "${PLATFORM}" == "s390x" ]; then
+		BASEIMAGE="s390x/clefos:7"
+	else
+		BASEIMAGE="${MULTIARCH_PREFIX}centos:7"
+	fi
+	DEVTOOLSET_ROOTPATH="/opt/rh/devtoolset-9/root"
+	PREPEND_PATH="${DEVTOOLSET_ROOTPATH}/usr/bin:"
+	if [ "${PLATFORM}" == "i686" ]; then
+		LD_LIBRARY_PATH_ARG="${DEVTOOLSET_ROOTPATH}/usr/lib:${DEVTOOLSET_ROOTPATH}/usr/lib/dyninst:/usr/local/lib"
+	else
+		LD_LIBRARY_PATH_ARG="${DEVTOOLSET_ROOTPATH}/usr/lib64:${DEVTOOLSET_ROOTPATH}/usr/lib:${DEVTOOLSET_ROOTPATH}/usr/lib64/dyninst:${DEVTOOLSET_ROOTPATH}/usr/lib/dyninst:/usr/local/lib64:/usr/local/lib"
+	fi
+else
+	echo "Unsupported policy: '${POLICY}'"
+	exit 1
+fi
+export BASEIMAGE
+export DEVTOOLSET_ROOTPATH
+export PREPEND_PATH
+export LD_LIBRARY_PATH_ARG
+
+docker build \
+	--build-arg POLICY --build-arg PLATFORM --build-arg BASEIMAGE \
+	--build-arg DEVTOOLSET_ROOTPATH --build-arg PREPEND_PATH --build-arg LD_LIBRARY_PATH_ARG \
+	--rm -t quay.io/pypa/${POLICY}_${PLATFORM}:${TRAVIS_COMMIT} \
+	-f docker/Dockerfile docker/
