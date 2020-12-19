4890d6771a95806fc711d512432fb68c7d449baf
mayeut <mayeut@users.noreply.github.com>
2020-12-19 18:31:42 +0100

    [manylinux2010] Use published base image for x86_64
    Use quay.io/pypa/manylinux2010_x86_64_centos6_no_vsyscall:2020-12-19-cd3e980 as a base image for manylinux2010_x86_64 now that this base image (centos6+patched glibc) has been published.
    
    Fix #878

--- build.sh
+++ build.sh
@@ -4,13 +4,4 @@
 set -ex
 
 
-if [ "$PLATFORM" == x86_64 ] && [ "$1" != glibc_skip ] || [ "$1" == glibc_only ]; then
-    docker/glibc/build.sh 32
-    docker/glibc/build.sh 64
-    if [ "$1" == "glibc_only" ]; then
-        exit 0
-    fi
-    docker/glibc/build.sh all
-fi
-
 docker build --rm -t "quay.io/pypa/manylinux2010_$PLATFORM:$TRAVIS_COMMIT" -f "docker/Dockerfile-$PLATFORM" docker/
