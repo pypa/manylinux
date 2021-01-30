f69edd808017915688eff5be0dbb065bfc4d4733
mayeut <mayeut@users.noreply.github.com>
2021-01-30 14:42:02 +0100

    Split cpython build from build_base image

--- docker/build_scripts/build-cpython.sh
+++ docker/build_scripts/build-cpython.sh
@@ -11,14 +11,6 @@ source $MY_DIR/build_env.sh
 # Get build utilities
 source $MY_DIR/build_utils.sh
 
-# Dependencies for compiling Python that we want to remove from
-# the final image after compiling Python
-PYTHON_COMPILE_DEPS="zlib-devel bzip2-devel expat-devel ncurses-devel readline-devel tk-devel gdbm-devel libdb-devel libpcap-devel xz-devel openssl-devel keyutils-libs-devel krb5-devel libcom_err-devel libidn-devel curl-devel perl-devel libffi-devel kernel-devel"
-CMAKE_DEPS="openssl-devel zlib-devel libcurl-devel"
-
-# Development tools and libraries
-yum -y install ${PYTHON_COMPILE_DEPS} ${CMAKE_DEPS}
-
 # Install a more recent SQLite3
 curl -fsSLO $SQLITE_AUTOCONF_DOWNLOAD_URL/$SQLITE_AUTOCONF_ROOT.tar.gz
 check_sha256sum $SQLITE_AUTOCONF_ROOT.tar.gz $SQLITE_AUTOCONF_HASH
