72685b1bcd1f2a468c5d56dc6f5f3a92a3377385
mayeut <mayeut@users.noreply.github.com>
2021-02-13 12:03:29 +0100

    Build arm64 & s390x on travis-ci
    Now that pypa has some credits on travis-ci again, move back arm64 & s390x on travis-ci where builds are done natively.
    
    ppc64le seems to have trouble with buildx for now so still on GHA

---
+++ travisci-install-buildx.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# This script is used to install docker buildx in travis-ci
+
+# Stop at any error, show all commands
+set -exuo pipefail
+
+BUILDX_MACHINE=$(uname -m)
+if [ ${BUILDX_MACHINE} == "x86_64" ]; then
+	BUILDX_MACHINE=amd64
+elif [ ${BUILDX_MACHINE} == "aarch64" ]; then
+	BUILDX_MACHINE=arm64
+fi
+
+mkdir -vp ~/.docker/cli-plugins/
+curl -sSL "https://github.com/docker/buildx/releases/download/v0.5.1/buildx-v0.5.1.linux-${BUILDX_MACHINE}" > ~/.docker/cli-plugins/docker-buildx
+chmod a+x ~/.docker/cli-plugins/docker-buildx
+docker buildx version
+docker buildx create --name builder-manylinux --driver docker-container --buildkitd-flags "--allow-insecure-entitlement security.insecure --allow-insecure-entitlement network.host" --use
+docker buildx inspect --bootstrap --builder builder-manylinux
