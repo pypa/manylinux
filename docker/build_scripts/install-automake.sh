6ae268c972ca080c08fb9fba237ed6d827395213
mayeut <mayeut@users.noreply.github.com>
2022-02-02 22:30:53 +0100

    fix: ignore `SIGPIPE` in previous piped command when `head -1` exits

--- docker/build_scripts/install-automake.sh
+++ docker/build_scripts/install-automake.sh
@@ -17,8 +17,10 @@ check_var ${AUTOMAKE_DOWNLOAD_URL}
 
 AUTOMAKE_VERSION=${AUTOMAKE_ROOT#*-}
 if automake --version > /dev/null 2>&1; then
-	INSTALLED=$(automake --version | head -1 | awk '{ print $NF }')
-	SMALLEST=$(echo -e "${INSTALLED}\n${AUTOMAKE_VERSION}" | sort -t. -k 1,1n -k 2,2n -k 3,3n -k 4,4n | head -1)
+	# || test $? -eq 141 is there to ignore SIGPIPE with set -o pipefail
+	# c.f. https://stackoverflow.com/questions/22464786/ignoring-bash-pipefail-for-error-code-141#comment60412687_33026977
+	INSTALLED=$((automake --version | head -1 || test $? -eq 141) | awk '{ print $NF }')
+	SMALLEST=$(echo -e "${INSTALLED}\n${AUTOMAKE_VERSION}" | sort -t. -k 1,1n -k 2,2n -k 3,3n -k 4,4n | head -1 || test $? -eq 141)
 	if [ "${SMALLEST}" == "${AUTOMAKE_VERSION}" ]; then
 		echo "skipping installation of automake ${AUTOMAKE_VERSION}, system provides automake ${INSTALLED}"
 		exit 0
