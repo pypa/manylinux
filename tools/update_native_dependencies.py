5983dd15c6be215383ad9baef0fef1674cb933c5
mayeut <mayeut@users.noreply.github.com>
2023-12-04 22:13:26 +0100

    fix: autoconf version check

--- tools/update_native_dependencies.py
+++ tools/update_native_dependencies.py
@@ -60,6 +60,9 @@ def _update_with_root(tool, dry_run):
     major = {
         "openssl": "3.0",
     }
+    only = {
+        "autoconf": "~v?[0-9]+\.[0-9]+(\.[0-9]+)?$",
+    }
     lines = DOCKERFILE.read_text().splitlines()
     re_ = re.compile(f"^RUN export {tool.upper()}_ROOT={tool}-(?P<version>\\S+) && \\\\$")
     for i in range(len(lines)):
@@ -67,7 +70,7 @@ def _update_with_root(tool, dry_run):
         if match is None:
             continue
         current_version = Version(match["version"], char_fix_required=tool=="openssl")
-        latest_version = latest(repo[tool], major=major.get(tool, None))
+        latest_version = latest(repo[tool], major=major.get(tool, None), only=only.get(tool, None))
         if latest_version > current_version:
             root = f"{tool}-{latest_version}"
             url = re.match(f"^    export {tool.upper()}_DOWNLOAD_URL=(?P<url>\\S+) && \\\\$", lines[i + 2])["url"]
