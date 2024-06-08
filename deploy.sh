2febd75fe20cb333b6930963caedfbc937ae144d
mayeut <mayeut@users.noreply.github.com>
2024-06-08 11:11:55 +0200

    chore: deploy with version like tags

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,25 @@
 #!/bin/bash
-tag="quay.io/pypa/${POLICY}_${PLATFORM}"
-build_id=$(git show -s --format=%cd-%h --date=short ${COMMIT_SHA})
+
+set -euo pipefail
+
+export TZ=UTC0
+
+TAG="quay.io/pypa/${POLICY}_${PLATFORM}"
+COMMIT_ABBREV_SHA=$(git show -s --format=%h ${COMMIT_SHA})
+COMMIT_DATE=$(git show -s --format=%cd --date=short ${COMMIT_SHA})
+BUILD_ID=${COMMIT_DATE}-${COMMIT_ABBREV_SHA}
+# Dependabot does not work with the BUILD_ID format
+# Use a version like tag
+if $(git rev-parse --is-shallow-repository); then
+  git fetch --shallow-since=${COMMIT_DATE}T00:00:00Z
+fi
+BUILD_NUMBER=$(git rev-list --since=${COMMIT_DATE}T00:00:00Z --count ${COMMIT_SHA})
+BUILD_ID2=${COMMIT_DATE//-/.}.${BUILD_NUMBER}
 
 docker login -u $QUAY_USERNAME -p $QUAY_PASSWORD quay.io
-docker tag ${tag}:${COMMIT_SHA} ${tag}:${build_id}
-docker tag ${tag}:${COMMIT_SHA} ${tag}:latest
-docker push ${tag}:${build_id}
-docker push ${tag}:latest
+docker tag ${TAG}:${COMMIT_SHA} ${TAG}:${BUILD_ID}
+docker tag ${TAG}:${COMMIT_SHA} ${TAG}:${BUILD_ID2}
+docker tag ${TAG}:${COMMIT_SHA} ${TAG}:latest
+docker push ${TAG}:${BUILD_ID}
+docker push ${TAG}:${BUILD_ID2}
+docker push ${TAG}:latest
