569fb52bd33790d8dcc66fce579ada1f395a5a28
mayeut <mayeut@users.noreply.github.com>
2024-06-08 13:08:50 +0200

    add --dry-run option to deploy

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,12 @@ set -euo pipefail
 
 export TZ=UTC0
 
+DRY_RUN=0
+if [ "${1:-}" == "--dry-run" ]; then
+  DRY_RUN=1
+  set -x
+fi
+
 TAG="quay.io/pypa/${POLICY}_${PLATFORM}"
 COMMIT_ABBREV_SHA=$(git show -s --format=%h ${COMMIT_SHA})
 COMMIT_DATE=$(git show -s --format=%cd --date=short ${COMMIT_SHA})
@@ -16,10 +22,13 @@ fi
 BUILD_NUMBER=$(git rev-list --since=${COMMIT_DATE}T00:00:00Z --count ${COMMIT_SHA})
 BUILD_ID2=${COMMIT_DATE//-/.}.${BUILD_NUMBER}
 
-docker login -u $QUAY_USERNAME -p $QUAY_PASSWORD quay.io
 docker tag ${TAG}:${COMMIT_SHA} ${TAG}:${BUILD_ID}
 docker tag ${TAG}:${COMMIT_SHA} ${TAG}:${BUILD_ID2}
 docker tag ${TAG}:${COMMIT_SHA} ${TAG}:latest
-docker push ${TAG}:${BUILD_ID}
-docker push ${TAG}:${BUILD_ID2}
-docker push ${TAG}:latest
+
+if [ $DRY_RUN -eq 0 ]; then
+  docker login -u $QUAY_USERNAME -p $QUAY_PASSWORD quay.io
+  docker push ${TAG}:${BUILD_ID}
+  docker push ${TAG}:${BUILD_ID2}
+  docker push ${TAG}:latest
+fi
