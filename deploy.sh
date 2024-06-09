71bcaec51d042ae4b9f7ebdeff9ae2b14dc47cbc
mayeut <mayeut@users.noreply.github.com>
2024-06-09 19:28:15 +0200

    fix: do not run deploy.sh --dry-run on PR

--- deploy.sh
+++ deploy.sh
@@ -7,8 +7,9 @@ export TZ=UTC0
 DRY_RUN=0
 if [ "${1:-}" == "--dry-run" ]; then
   DRY_RUN=1
-  set -x
 fi
+
+set -x
 
 TAG="quay.io/pypa/${POLICY}_${PLATFORM}"
 COMMIT_ABBREV_SHA=$(git show -s --format=%h ${COMMIT_SHA})
@@ -25,6 +26,8 @@ BUILD_ID2=${COMMIT_DATE//-/.}.${BUILD_NUMBER}
 docker tag ${TAG}:${COMMIT_SHA} ${TAG}:${BUILD_ID}
 docker tag ${TAG}:${COMMIT_SHA} ${TAG}:${BUILD_ID2}
 docker tag ${TAG}:${COMMIT_SHA} ${TAG}:latest
+
+set +x
 
 if [ $DRY_RUN -eq 0 ]; then
   docker login -u $QUAY_USERNAME -p $QUAY_PASSWORD quay.io
