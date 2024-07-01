7beb9ae220bcf3da425d323817709c1a1e2bd35d
mayeut <mayeut@users.noreply.github.com>
2024-07-01 20:40:29 +0200

    fix: move to vault.centos.org for CentOS 7 mirrors

--- docker/build_scripts/fixup-mirrors.sh
+++ docker/build_scripts/fixup-mirrors.sh
@@ -3,3 +3,13 @@
 
 # Stop at any error, show all commands
 set -exuo pipefail
+if [ "${AUDITWHEEL_POLICY}" == "manylinux2014" ] && [ "${AUDITWHEEL_ARCH}" != "s390x" ]; then
+	# Centos 7 is EOL and is no longer available from the usual mirrors, so switch
+	# to https://vault.centos.org
+	sed -i 's/enabled=1/enabled=0/g' /etc/yum/pluginconf.d/fastestmirror.conf
+	sed -i 's/^mirrorlist/#mirrorlist/g' /etc/yum.repos.d/*.repo
+	sed -i 's;^.*baseurl=http://mirror;baseurl=https://vault;g' /etc/yum.repos.d/*.repo
+	if [ "${AUDITWHEEL_ARCH}" == "aarch64" ] || [ "${AUDITWHEEL_ARCH}" == "ppc64le" ]; then
+		sed -i 's;/centos/7/;/altarch/7/;g' /etc/yum.repos.d/*.repo
+	fi
+fi
