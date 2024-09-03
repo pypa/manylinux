caba29c12687d8fd84fd585e5a5b9e4116ab385f
mayeut <mayeut@users.noreply.github.com>
2024-09-03 18:45:44 +0200

    Only install libxcrypt on manylinux2014

--- docker/build_scripts/install-libxcrypt.sh
+++ docker/build_scripts/install-libxcrypt.sh
@@ -10,31 +10,9 @@ MY_DIR=$(dirname "${BASH_SOURCE[0]}")
 # Get build utilities
 source $MY_DIR/build_utils.sh
 
-if [ "$BASE_POLICY" == "musllinux" ]; then
-	echo "Skip libxcrypt installation on musllinux"
+if [ "${AUDITWHEEL_POLICY}" != "manylinux2014" ]; then
+	echo "Skip libxcrypt installation on ${AUDITWHEEL_POLICY}"
 	exit 0
-elif [ "${AUDITWHEEL_POLICY}" == "manylinux_2_28" ]; then
-	echo "Skip libxcrypt installation on manylinux_2_28"
-	exit 0
-fi
-
-# We need perl 5.14+
-if ! perl -e 'use 5.14.0' &> /dev/null; then
-	check_var ${PERL_ROOT}
-	check_var ${PERL_HASH}
-	check_var ${PERL_DOWNLOAD_URL}
-	fetch_source ${PERL_ROOT}.tar.gz ${PERL_DOWNLOAD_URL}
-	check_sha256sum "${PERL_ROOT}.tar.gz" "${PERL_HASH}"
-
-	tar -xzf ${PERL_ROOT}.tar.gz
-	pushd ${PERL_ROOT}
-	./Configure -des -Dprefix=/tmp/perl-libxcrypt > /dev/null
-	make -j$(nproc) > /dev/null
-	make install > /dev/null
-	popd
-
-	rm -rf ${PERL_ROOT}.tar.gz ${PERL_ROOT}
-	export PATH=/tmp/perl-libxcrypt/bin:${PATH}
 fi
 
 # Install libcrypt.so.1 and libcrypt.so.2
@@ -77,8 +55,3 @@ rm -rf /manylinux-rootfs
 rm -rf /usr/include/crypt.h
 find /lib* /usr/lib* \( -name 'libcrypt.a' -o -name 'libcrypt.so' -o -name 'libcrypt.so.*' -o -name 'libcrypt-2.*.so' \) -delete
 ldconfig
-
-# Remove temp Perl
-if [ -d /tmp/perl-libxcrypt ]; then
-	rm -rf /tmp/perl-libxcrypt
-fi
