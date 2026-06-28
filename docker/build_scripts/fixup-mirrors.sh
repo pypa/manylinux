dae94137841fb853e687ed31a7b3991259f97f62
mayeut <mayeut@users.noreply.github.com>
2026-06-28 19:06:54 +0200

    fix: add baseurl to manylinux2014_s390x repos
    this should help while mirror fetching is down

--- docker/build_scripts/fixup-mirrors.sh
+++ docker/build_scripts/fixup-mirrors.sh
@@ -3,13 +3,26 @@
 
 # Stop at any error, show all commands
 set -exuo pipefail
-if [ "${AUDITWHEEL_POLICY}" == "manylinux2014" ] && [ "${AUDITWHEEL_ARCH}" != "s390x" ]; then
-	# Centos 7 is EOL and is no longer available from the usual mirrors, so switch
-	# to https://vault.centos.org
-	sed -i 's/enabled=1/enabled=0/g' /etc/yum/pluginconf.d/fastestmirror.conf
-	sed -i 's/^mirrorlist/#mirrorlist/g' /etc/yum.repos.d/*.repo
-	sed -i 's;^.*baseurl=http://mirror;baseurl=https://vault;g' /etc/yum.repos.d/*.repo
-	if [ "${AUDITWHEEL_ARCH}" == "aarch64" ] || [ "${AUDITWHEEL_ARCH}" == "ppc64le" ]; then
-		sed -i 's;/centos/7/;/altarch/7/;g' /etc/yum.repos.d/*.repo
+if [ "${AUDITWHEEL_POLICY}" == "manylinux2014" ]; then
+	if [ "${AUDITWHEEL_ARCH}" == "s390x" ]; then
+		yum-config-manager --setopt=os.baseurl=https://download.sinenomine.net/clefos/7/os --save
+		yum-config-manager --setopt=updates.baseurl=https://download.sinenomine.net/clefos/7/updates --save
+		yum-config-manager --setopt=extras.baseurl=https://download.sinenomine.net/clefos/7/extras --save
+		yum-config-manager --setopt=centosplus.baseurl=https://download.sinenomine.net/clefos/7/centosplus --save
+		if [ -f /etc/yum.repos.d/CentOS-SCLo-scl-rh.repo ]; then
+			yum-config-manager --setopt=clefos-rh.baseurl=https://download.sinenomine.net/clefos/7/sclo/s390x/rh --save
+		fi
+		if [ -f /etc/yum.repos.d/epel.repo ]; then
+			yum-config-manager --setopt=epel.baseurl=https://download.sinenomine.net/clefos/epel7 --save
+		fi
+	else
+		# Centos 7 is EOL and is no longer available from the usual mirrors, so switch
+		# to https://vault.centos.org
+		sed -i 's/enabled=1/enabled=0/g' /etc/yum/pluginconf.d/fastestmirror.conf
+		sed -i 's/^mirrorlist/#mirrorlist/g' /etc/yum.repos.d/*.repo
+		sed -i 's;^.*baseurl=http://mirror;baseurl=https://vault;g' /etc/yum.repos.d/*.repo
+		if [ "${AUDITWHEEL_ARCH}" == "aarch64" ] || [ "${AUDITWHEEL_ARCH}" == "ppc64le" ]; then
+			sed -i 's;/centos/7/;/altarch/7/;g' /etc/yum.repos.d/*.repo
+		fi
 	fi
 fi
